2eebb259701a23f4d5e66e831fa9a8ee4b703891
Карпин Кирилл <kirill_karpin@hotmail.com>
2025-04-26 21:03:29 +0500

    feat: исправление замечаний

--- server/src/main/java/ru/practicum/shareit/Config.java
+++ server/src/main/java/ru/practicum/shareit/Config.java
@@ -2,7 +2,8 @@ package ru.practicum.shareit;
 
 public final class Config {
 
-  private Config() {}
+  private Config() {
+  }
 
   public static final String USER_ID_HEADER = "X-Sharer-User-Id";
 }
